740d7ae4cf7ffa1172dc2ac0673b633f2b9b8021
VadimStarv <105434892+VadimStarv@users.noreply.github.com>
2022-09-23 18:16:46 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-let headerList = document.querySelector('.special__li');
-headerList.addEventListener('click',function(){
-  let ourHeader = document.querySelector('.about__header');
-  ourHeader.classList.toggle('active');
-})
-
-
-
